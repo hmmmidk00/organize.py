f7e1b04265ef8ab2dfd483b1d2553d140a913c27
hmmmidk00 <78182552+hmmmidk00@users.noreply.github.com>
2021-01-30 17:35:55 +0100

    Update organiser.py

--- organiser.py
+++ organiser.py
@@ -1,4 +1,20 @@
 import os
+import shutil
+#os.listdir()--> list all files on that directory
 for item in os.listdir():
+	#get the name and the extension of a file
 	filename, file_extension = os.path.splitext(item)
-    print(os.listdir())
+	if file_extension==".pdf":
+		#move item to pdf directory
+		shutil.move(f"C:/Users/root/Downloads/{item}", f"C:/Users/root/Downloads/pdf/{item}")
+	if file_extension==".mp4":
+		#move item to mp4 directory
+		shutil.move(f"C:/Users/root/Downloads/{item}", f"C:/Users/root/Downloads/mp4/{item}")
+	if file_extension==".exe":
+		shutil.move(f"C:/Users/root/Downloads/{item}", f"C:/Users/root/Downloads/exe/{item}")
+	if file_extension==".txt":
+		shutil.move(f"C:/Users/root/Downloads/{item}", f"C:/Users/root/Downloads/txt/{item}")
+	if file_extension==".iso" or file_extension=="ISO" :
+		shutil.move(f"C:/Users/root/Downloads/{item}", f"C:/Users/root/Downloads/iso/{item}")
+
+
